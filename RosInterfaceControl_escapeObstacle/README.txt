c5f375b7774b77c13609bec17f56dec0663dc084
“Austin <valour8@hotmail.com>
2018-06-22 22:32:59 +0900

    add the ttt file for v-rep, and update README

--- RosInterfaceControl_escapeObstacle/README.txt
+++ RosInterfaceControl_escapeObstacle/README.txt
@@ -12,3 +12,10 @@ ROS Kinetic, V-REP 3.5.0 rev4, Ubuntu 16.04
 This is just the the first try to use ROS to control the robot in V-REP environments.
 
 
+lua code:
+bubbleRos1.lua   -> lua code for object bubbleRos1, you also can see inthe escapeObstacle.ttt
+bubbleRos2.lua	 -> lua code for object bubbleRos2, you also can see inthe escapeObstacle.ttt
+
+obstacle1.lua	 -> lua code for object obstacle1, you also can see inthe escapeObstacle.ttt
+obstacle2.lua	 -> lua code for object obstacle2, you also can see inthe escapeObstacle.ttt
+obstacle3.lua	 -> lua code for object obstacle3, you also can see inthe escapeObstacle.ttt
---
+++ RosInterfaceControl_escapeObstacle/bubbleRos2.lua
@@ -0,0 +1,71 @@
+function sysCall_init()
+        
+    robotHandle=sim.getObjectAssociatedWithScript(sim.handle_self)
+    leftMotor=sim.getObjectHandle("bubbleRob_leftMotor2") -- Handle of the left motor
+    rightMotor=sim.getObjectHandle("bubbleRob_rightMotor2") -- Handle of the right motor
+    noseSensor=sim.getObjectHandle("bubbleRob_sensingNose2") -- Handle of the pro sensor
+    -- Check if the required ROS plugin is there:
+    moduleName=0
+    moduleVersion=0
+    index=0
+    pluginNotFound=true
+    while moduleName do
+        moduleName,moduleVersion=sim.getModuleName(index)
+        if (moduleName=='RosInterface') then
+            pluginNotFound=false
+        end
+        index=index+1
+    end
+    -- Ok now launch the ROS client application:
+    if (not pluginNotFound) then
+        local sysTime=sim.getSystemTimeInMs(-1) 
+        local leftMotorTopicName='leftMotorSpeed'..sysTime
+        local rightMotorTopicName='rightMotorSpeed'..sysTime
+        local sensorTopicName='sensorTrigger'..sysTime
+        local simulationTimeTopicName='simTime'..sysTime
+        -- Prepare the sensor publisher and the motor speed subscribers:
+        sensorPub=simROS.advertise('/'..sensorTopicName,'std_msgs/Bool')
+        simTimePub=simROS.advertise('/'..simulationTimeTopicName,'std_msgs/Float32')
+        leftMotorSub=simROS.subscribe('/'..leftMotorTopicName,'std_msgs/Float32','setLeftMotorVelocity_cb')
+        rightMotorSub=simROS.subscribe('/'..rightMotorTopicName,'std_msgs/Float32','setRightMotorVelocity_cb')
+        -- Now we start the client application:
+        result=sim.launchExecutable('rosBubbleRob2',leftMotorTopicName.." "..rightMotorTopicName.." "..sensorTopicName.." "..simulationTimeTopicName,0)
+    end
+    
+end
+function setLeftMotorVelocity_cb(msg)
+    -- Left motor speed subscriber callback
+
+end
+
+function setRightMotorVelocity_cb(msg)
+    -- Right motor speed subscriber callback
+
+end
+
+function getTransformStamped(objHandle,name,relTo,relToName)
+
+end
+
+
+function sysCall_actuation()
+    -- Send an updated sensor and simulation time message, and send the transform of the robot:
+    if not pluginNotFound then
+        local result=sim.readProximitySensor(noseSensor)
+        local detectionTrigger={}
+        detectionTrigger['data']=result>0
+
+        -- Send the robot's transform:
+        simROS.sendTransform(getTransformStamped())
+        -- To send several transforms at once, use simROS.sendTransforms instead
+    end
+end
+
+function sysCall_cleanup()
+    if not pluginNotFound then
+        -- Following not really needed in a simulation script (i.e. automatically shut down at simulation end):
+        simROS.shutdownPublisher()
+        simROS.shutdownSubscriber()
+        simROS.shutdownSubscriber()
+    end
+end
